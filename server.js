f670c61314cd997627b9fd6caeb5bda776688fb1
Rebecca Ariss <rebeccamarionruth@gmail.com>
2022-01-13 15:01:34 -0500

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
     client: 'pg',
     connection: {
         // host : '127.0.0.1', // (localhost)
-        host : 'postgresql-fitted-80755'
+        host : 'agile-plateau-47799'
         port : 5432,
         user : '',
         password : '',
